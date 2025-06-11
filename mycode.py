4866a95321084d6fb747aded6f49db73be7c30f4
omnarayansharma777 <sharma1380992@gmail.com>
2025-06-11 14:16:04 +0530

    added data and mycode.py

---
+++ mycode.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import os
+data={'name': ['Alice', 'Bob', 'Charlie'],'age': [25, 30, 35],'city': ['New York', 'Los Angeles', 'Chicago']}
+df=pd.DataFrame(data)
+data_dir='data'
+os.makedirs(data_dir,exist_ok=True)
+file_path=os.path.join(data_dir,'data.csv')
+df.to_csv(file_path,index=False)
