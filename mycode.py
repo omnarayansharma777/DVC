ca8e876a97166539f3215005377994cce50926ac
omnarayansharma777 <sharma1380992@gmail.com>
2025-06-11 14:59:51 +0530

    changed data and file

--- mycode.py
+++ mycode.py
@@ -2,6 +2,8 @@ import pandas as pd
 import os
 data={'name': ['Alice', 'Bob', 'Charlie'],'age': [25, 30, 35],'city': ['New York', 'Los Angeles', 'Chicago']}
 df=pd.DataFrame(data)
+new_row={'name': 'David', 'age': 40, 'city': 'Houston'}
+df=df._append(new_row, ignore_index=True)
 data_dir='data'
 os.makedirs(data_dir,exist_ok=True)
 file_path=os.path.join(data_dir,'data.csv')
